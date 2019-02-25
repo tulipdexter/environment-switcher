de30c7765a32a6af028bb1cc993a7352b8883f4c
David Paul <david.paul@springernature.com>
2019-02-25 16:57:24 +0000

    Different syntax for HOC

--- src/options/js/components/SiteLoader.js
+++ src/options/js/components/SiteLoader.js
@@ -3,37 +3,34 @@ import {h, render, Component} from 'preact';
 // Higher order component
 // See https://daveceddia.com/extract-state-with-higher-order-components/
 // This might be moved to a 'lib' dir so it can be used by the browser tool as well
-export const siteLoader = () => {
-    return WrappedComponent => {
-        return class SiteLoader extends Component {
-            constructor(props) {
-                console.log('here');
-                super(props);
+export const siteLoader = WrappedComponent => {
+    return class SiteLoader extends Component {
+        constructor(props) {
+            super(props);
 
-                this.state = {
-                    loaded: false,
-                    sites: null
-                }
+            this.state = {
+                loaded: false,
+                sites: null
             }
+        }
 
-            componentDidMount() {
-                chrome.storage.sync.get('sites', sites => {
-                    this.setState({
-                        loaded: true,
-                        sites: sites
-                    });
+        componentDidMount() {
+            chrome.storage.sync.get('sites', sites => {
+                this.setState({
+                    loaded: true,
+                    sites: sites
                 });
+            });
+        }
+
+        render(props, {loaded, sites}) {
+            if (!loaded) {
+                return <div>Loading...</div>;
             }
 
-            render(props, {loaded, sites}) {
-                if (!loaded) {
-                    return <div>Loading...</div>;
-                }
-
-                return (
-                    <WrappedComponent sites={sites} {...props} />
-                )
-            }
+            return (
+                <WrappedComponent sites={sites} {...props} />
+            )
         }
     }
 };
--- src/options/js/components/display.js
+++ src/options/js/components/display.js
@@ -4,7 +4,7 @@ import {NoSites} from "./no-sites/no-sites";
 
 // siteLoader returns a function that takes a Component
 // and then returns a wrapper component
-export const Display = siteLoader()(props => {
+export const Display = siteLoader(props => {
     if (props.sites.length > 0) {
         return (<p>Site list will go here</p>);
     } else {
